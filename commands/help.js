b7cf14f9a5e7ac87ff574e606b745e6309efa662
AdSinghhh <adarshingh0604@gmail.com>
2023-05-05 01:34:53 -0700

    project init

---
+++ commands/help.js
@@ -0,0 +1,13 @@
+//help function will list all the ways by which u can run the command 
+
+function helpFn(){
+    console.log(`List of all the commands 
+                  (1) Tree - node fo.js tree <dirPath>
+                  (2) Organize - node fo.js organize <dirPath>
+                  (3) Help - node fo.js help`)
+
+}
+
+module.export = {
+    helpFnKey : helpFn
+}
